f3e3be511c94b21d5ecd27b3683263aa79111625
Simone Knowles <tigsye@hotmail.com>
2020-01-15 14:09:07 +0000

    .

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 
 export default class Header extends Component {
 
+
+    homeClicked = (event) => {
+        this.props.history.push('./');
+    }
     render(){
         return (
             <div>
-                <button className='homebutton' onClick={this.homeClicked} href="/">Home</button>
+                <button className='homebutton' onClick={this.homeClicked} >Home</button>
                 <h1>BOOK AN EVENT</h1>
             </div>
         );
